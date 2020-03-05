83e0063633bdece194a6a5157b1bcf7a0d9748f5
Felix Green <work@felix.green>
2020-03-05 15:40:54 -0500

    🚀 initial commit

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+// import url from '@rollup/plugin-url';
+
+// export default {
+//   input: 'src/index.js',
+//   output: {
+//     dir: 'output',
+//     format: 'cjs'
+//   },
+//   plugins: [url()]
+// };
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// import './colors.css';
+
+export { default as tailwindConfig } from './tailwindconfig';
+
+// export const sum = (a: number, b: number) => {
+//   if ('development' === process.env.NODE_ENV) {
+//     console.log('boop');
+//   }
+//   return a + b;
+// };
